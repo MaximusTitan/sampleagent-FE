f5e27965c1736461f7b8f2d1eea82274a843b0e1
Shrideep_Tamboli <27.tshrideep@gmail.com>
2024-12-12 14:21:20 +0530

    Prod_FIX_1.1

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import ClerkClientProvider from "./ClerkClientProvider";
 import { FileProvider } from '@/context/FileContext';
